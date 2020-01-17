10be1188a662be6b8daa160d52716fca90ee563e
Thiago Nelsi do Couto <thiagonelsi@yahoo.com.br>
2020-01-17 22:35:50 +0000

    Dev icon added

--- mobile/src/pages/Main.js
+++ mobile/src/pages/Main.js
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from 'react'
-import { StyleSheet } from 'react-native'
-import MapView from 'react-native-maps'
+import { StyleSheet, Image, View, Text } from 'react-native'
+import MapView, { Marker, Callout } from 'react-native-maps'
 import { requestPermissionsAsync, getCurrentPositionAsync } from 'expo-location'
 
-function Main() {
+function Main({ navigation }) {
 
     const [currentRegion, setCurrentRegion] = useState(null)
 
@@ -37,12 +37,49 @@ function Main() {
         return null
     }
 
-    return <MapView initialRegion={currentRegion} style={styles.map}  />
+    return (
+        <MapView initialRegion={currentRegion} style={styles.map}>
+            <Marker coordinate={{latitude: currentRegion.latitude, longitude: currentRegion.longitude}}>
+                <Image style={styles.avatar} source={{ uri: 'https://avatars2.githubusercontent.com/u/52456089?s=460&v=4' }} />
+            
+                <Callout>
+                    <View style={styles.callout}>
+                        <Text style={styles.devName}>Thiago Nelsi do Couto</Text>
+                        <Text style={styles.devBio}>Computer student at "IFSULDEMINAS Campus Inconfidentes" and a super technology lover! desktop_computer</Text>
+                        <Text style={styles.devTechs}>Node.js, ReactJS, React Native, Python, C</Text>
+                    </View>
+                </Callout>
+            </Marker>
+        </MapView>
+    )
+
+
 }
 
 const styles = StyleSheet.create({
     map: {
         flex: 1,
+    },
+    avatar: {
+        width: 45,
+        height: 45,
+        borderRadius: 4,
+        borderWidth: 1,
+        borderColor: '#7d40e7'
+    },
+    callout: {
+        width: 260,
+    },
+    devName: {
+        fontWeight: 'bold',
+        fontSize: 16,
+    },
+    devBio: {
+        color: '#666',
+        marginTop: 5
+    },
+    devTechs: {
+        marginTop: 5
     }
 })
 
--- mobile/src/routes.js
+++ mobile/src/routes.js
@@ -1,8 +1,8 @@
 import { createAppContainer } from 'react-navigation'
 import { createStackNavigator } from 'react-navigation-stack'
 
-import Main from './Main'
-import Profile from './Profile'
+import Main from './pages/Main'
+import Profile from './pages/Profile'
 
 const Routes = createAppContainer(
     createStackNavigator({
